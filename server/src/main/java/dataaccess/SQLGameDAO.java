b0f71114f41fb9816966f35eb77d1c8666be3953
Will Larsen <larsenwa@gmail.com>
2025-03-12 09:02:35 -0600

    Created base SQL_DAO class

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -13,10 +13,7 @@ import java.util.HashMap;
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import static java.sql.Types.NULL;
 
-public class SQLGameDAO implements GameDAO{
-
-    final private HashMap<Integer,GameData> gameMap = new HashMap<>();
-
+public class SQLGameDAO extends SQL_DAO implements GameDAO {
 
     @Override
     public void createGame(GameData g) throws DataAccessException {
@@ -107,28 +104,5 @@ public class SQLGameDAO implements GameDAO{
     public void clear() throws DataAccessException {
         var statement = "TRUNCATE GameData";
         executeUpdate(statement);
-    }
-
-    private int executeUpdate(String statement, Object... params) throws DataAccessException {
-        try (var conn = DatabaseManager.getConnection()) {
-            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    if (param instanceof String p) ps.setString(i + 1, p);
-                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
-                    else if (param == null) ps.setNull(i + 1, NULL);
-                }
-                ps.executeUpdate();
-
-                var rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    return rs.getInt(1);
-                }
-
-                return 0;
-            }
-        } catch (SQLException e) {
-            throw new DataAccessException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
-        }
     }
 }
--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,19 +1,42 @@
 package dataaccess;
 
+import model.AuthData;
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.util.HashMap;
 
-public class SQLUserDAO implements UserDAO{
+public class SQLUserDAO extends SQL_DAO implements UserDAO {
 
 
     @Override
     public void createUser(UserData u) throws DataAccessException {
-
+        try {
+            var statement = "INSERT INTO UserData (username, password, email) VALUES (?, ?, ?)";
+            String username = u.username();
+            String hashedPassword = BCrypt.hashpw(u.password(), BCrypt.gensalt());
+            String email = u.email();
+            executeUpdate(statement, username, hashedPassword, email);
+        } catch (DataAccessException e) {
+            throw new DataAccessException(500,e.getMessage());
+        }
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username FROM UserData WHERE username=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readUser(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
